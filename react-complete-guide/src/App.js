934ee88248a145b95bd5610cf3ec39f4ce54a684
agdavid <antonio.g.david@gmail.com>
2018-09-07 17:22:49 -0400

    render Person props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-
+ 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
-        <Person />
+        <Person name="Max" age="21"/>
+        <Person name="Anne" age="23"> This is a child prop </Person>
       </div>
     );
   }
