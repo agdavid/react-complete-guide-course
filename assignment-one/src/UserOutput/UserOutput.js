f87109a0cda450a410fb454bf31cb9452981fa2e
agdavid <antonio.g.david@gmail.com>
2018-09-09 17:46:24 -0400

    add UserInput and UserOutput components

---
+++ assignment-one/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Paragraph One</p>
+            <p>Paragraph Two</p>
+        </div>
+    )
+}
+
+export default userOutput;
