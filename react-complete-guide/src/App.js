32b9a5f95db21bd597ed5b43beb63995541f7a89
agdavid <antonio.g.david@gmail.com>
2018-09-09 17:12:40 -0400

    reference function in props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -20,17 +20,37 @@ class App extends Component {
     ],
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log("Was clicked!");
+    this.setState({
+      persons: [
+        {
+          name: newName,
+          age: 21
+        },
+        {
+          name: 'Manu',
+          age: 29
+        },
+        {
+          name: 'Stephanie',
+          age: 26
+        }
+      ]
+    });
   };
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('New Max!!')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'New Max Again')}
+        />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
