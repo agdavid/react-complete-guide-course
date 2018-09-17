7315d4e73badd2cfafb411f8e244ca863e2f3462
agdavid <antonio.g.david@gmail.com>
2018-09-17 06:41:48 -0400

    refactor rendered JSX into variable

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -77,14 +77,11 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <button style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-        </button>
-        { this.state.showPersons ?
-          <div>
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person 
             name={this.state.persons[1].name} 
@@ -93,8 +90,17 @@ class App extends Component {
             changed={this.nameChangeHandler}
           />
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        </div> : null
-        }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
