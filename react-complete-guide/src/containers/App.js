1ad8377e2027fed053bab1f159c535bfe84a200b
agdavid <antonio.g.david@gmail.com>
2018-09-19 07:26:46 -0400

    render Person in new Persons component

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 import '../components/Persons/Person/Person.css';
  
 class App extends Component {
@@ -71,14 +71,11 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map( (person, index) => {
-            return <Person 
-              name={person.name} 
-              age={person.age}
-              key={person.id} 
-              click={() => this.deletePersonHandler(index)}
-              changed={(event) => this.nameChangeHandler(event, person.id)}/>
-          })}
+          <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangeHandler}
+          />
         </div>
       );
     }
