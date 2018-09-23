3bba408ce606f0184f1ad039dc58d535eebc0280
agdavid <antonio.g.david@gmail.com>
2018-09-23 19:54:33 -0400

    turn functional components into stateful components

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css';
 
-const person = (props) => {
-    return (
-        <div onClick={props.click} className="Person">
-            <p>I'm {props.name} and {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div> 
-    );
-};
+class Person extends Component {
+    render() {
+        return (
+            <div onClick={this.props.click} className="Person">
+                <p>I'm {this.props.name} and {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        );
+    }
+}
 
-export default person;
+export default Person;
