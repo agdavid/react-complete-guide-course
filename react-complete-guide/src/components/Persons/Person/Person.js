e55c428a5fa4fe43d7177fbd6c2efa054c4f630c
agdavid <antonio.g.david@gmail.com>
2018-09-24 08:11:19 -0400

    install and use prop-types for validation

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Person.css';
 
 class Person extends Component {
@@ -12,5 +13,12 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func, 
+    name: PropTypes.string, 
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default Person;
